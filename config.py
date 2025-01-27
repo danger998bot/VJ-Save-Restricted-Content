fbf02a7a1a689903164ac118d130122347fc0f0a
danger998bot <sibasahu036@gmail.com>
2025-01-27 19:37:31 +0530

    Update config.py

--- config.py
+++ config.py
@@ -4,16 +4,18 @@ import os
 BOT_TOKEN = os.environ.get("BOT_TOKEN", "")
 
 # Your API ID from my.telegram.org
-API_ID = int(os.environ.get("API_ID", "22336285"))
+API_ID = int(os.environ.get("API_ID", "16439444"))
 
 # Your API Hash from my.telegram.org
-API_HASH = os.environ.get("API_HASH", "3d84051d72e78013f6b38a1b7e26e469")
+API_HASH = os.environ.get("API_HASH", "98cc0614996ec24abefbdeb3d0fbaea1")
 
 # Your Owner / Admin Id For Broadcast 
-ADMINS = int(os.environ.get("ADMINS", "7547798233"))
+ADMINS = int(os.environ.get("ADMINS", "7054450549"))
 
 # Your Mongodb Database Url
-DB_URI = os.environ.get("DB_URI", "mongodb+srv://sibasahu036:f28evZw6b5K0JfeK@cluster0.7qoey.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0")
+DB_URI = os.environ.get("DB_URI", "mongodb+srv://sibasahu036:U2SRRz3W1Q7lZLwK@cluster0.7qoey.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0
+
+")
 DB_NAME = os.environ.get("DB_NAME", "vjsavecontentbot")
 
 # If You Want Error Message In Your Personal Message Then Turn It True Else If You Don't Want Then Flase
