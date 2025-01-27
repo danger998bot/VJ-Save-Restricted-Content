808e5cc77b1ff6508007ea8dda7ae2ceb8398d29
danger998bot <sibasahu036@gmail.com>
2025-01-27 20:06:06 +0530

    Update start.py

--- TechVJ/start.py
+++ TechVJ/start.py
@@ -61,10 +61,10 @@ async def send_start(client: Client, message: Message):
     if not await db.is_user_exist(message.from_user.id):
         await db.add_user(message.from_user.id, message.from_user.first_name)
     buttons = [[
-        InlineKeyboardButton("❣️ Developer", url = "https://t.me/Danger9938")
+        InlineKeyboardButton("❣️ Developer", url = "https://t.me/Mrdanger9938")
     ],[
-        InlineKeyboardButton('🔍 sᴜᴘᴘᴏʀᴛ ɢʀᴏᴜᴘ', url='https://t.me/danger_chart_group'),
-        InlineKeyboardButton('🤖 ᴜᴘᴅᴀᴛᴇ ᴄʜᴀɴɴᴇʟ', url='https://t.me/danger_chart_group')
+        InlineKeyboardButton('🔍 sᴜᴘᴘᴏʀᴛ ɢʀᴏᴜᴘ', url='https://t.me/dangerbotking'),
+        InlineKeyboardButton('🤖 ᴜᴘᴅᴀᴛᴇ ᴄʜᴀɴɴᴇʟ', url='https://t.me/dangerbotking')
     ]]
     reply_markup = InlineKeyboardMarkup(buttons)
     await client.send_message(
